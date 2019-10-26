b04a4a368facf2ab69baa03d4073e02ee2906b6f
Matheus Duarte <matheusdcarvalho007@gmail.com>
2019-10-26 00:43:33 -0300

    Add progress bar

--- src/js/CountDown.jsx
+++ src/js/CountDown.jsx
@@ -4,6 +4,7 @@ import Grid from '@material-ui/core/Grid';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import Typography from '@material-ui/core/Typography';
+import LinearProgress from '@material-ui/core/LinearProgress';
 
 export default class CountDown extends React.Component {
     
@@ -15,7 +16,9 @@ export default class CountDown extends React.Component {
                 hours: 0,
                 minutes: 0,
                 seconds: 0
-            }
+            },
+            partyDay: 0,
+            currentDay: 0
         };
         this.calculateRemainingTime = this.calculateRemainingTime.bind(this);
     } 
@@ -27,10 +30,12 @@ export default class CountDown extends React.Component {
     calculateRemainingTime() {
         var remainingTime = {};
         let diffTime = 0;
-        let dateResenha = new Date(2019, 10, 31, 17, 0, 0, 0);
-        let dataAtual = new Date();
-        diffTime  = ( Math.abs(dateResenha - dataAtual) ) / 1000;
-        
+        const partyDate = new Date(2019, 10, 31, 17, 0, 0, 0);
+        const currentDate = new Date();
+        const currentDay  = ( Math.abs(currentDate) ) / 1000;
+        const partyDay  = ( Math.abs(partyDate) ) / 1000;
+        diffTime  = ( Math.abs(partyDate - currentDate) ) / 1000;
+
         remainingTime.seconds = parseInt( diffTime % 60 );
         diffTime /= 60;
 
@@ -39,8 +44,8 @@ export default class CountDown extends React.Component {
 
         remainingTime.hours = parseInt( diffTime % 24 );
         remainingTime.days = parseInt( diffTime / 24 );
-        
-        this.setState({ ...this.state, remainingTime: remainingTime});
+
+        this.setState({ ...this.state, remainingTime, partyDay, currentDay});
         
         setTimeout(() => this.calculateRemainingTime(), 1000);
         
@@ -48,60 +53,67 @@ export default class CountDown extends React.Component {
 
     render() {
         return (
-            <Grid container spacing={2} alignItems="center">
-                <Grid item xs={12} md={3}>
-                    <Card className="time-card">
-                        <CardContent className = "time-card">
-                            <Typography variant="h4" className="time-card-time">
-                                { this.state.remainingTime.days} 
-                            </Typography>
-                            <Typography>
-                                Dias
-                            </Typography>
-                        </CardContent>
-                    </Card>
-                </Grid>
-                
-                <Grid item xs={12} md={3}>
-                    <Card className="time-card">
-                        <CardContent className = "time-card">
-                            <Typography variant="h4" gutterBottom>
-                                { this.state.remainingTime.hours} 
-                            </Typography>
-                            <Typography>
-                                Horas
-                            </Typography>
-                        </CardContent>
-                    </Card>
-                </Grid>
+            <div>
+                <Grid container spacing={2} alignItems="center">
+                    <Grid item xs={12} md={3}>
+                        <Card className="time-card">
+                            <CardContent className = "time-card">
+                                <Typography variant="h4" className="time-card-time">
+                                    { this.state.remainingTime.days} 
+                                </Typography>
+                                <Typography>
+                                    Dias
+                                </Typography>
+                            </CardContent>
+                        </Card>
+                    </Grid>
+                    
+                    <Grid item xs={12} md={3}>
+                        <Card className="time-card">
+                            <CardContent className = "time-card">
+                                <Typography variant="h4" gutterBottom>
+                                    { this.state.remainingTime.hours} 
+                                </Typography>
+                                <Typography>
+                                    Horas
+                                </Typography>
+                            </CardContent>
+                        </Card>
+                    </Grid>
 
-                <Grid item xs={12} md={3}>  
-                    <Card className="time-card">
-                        <CardContent className = "time-card">
-                            <Typography variant="h4" gutterBottom>
-                                { this.state.remainingTime.minutes} 
-                            </Typography>
-                            <Typography>
-                                Minutos
-                            </Typography>
-                        </CardContent>
-                    </Card>
-                </Grid>
+                    <Grid item xs={12} md={3}>  
+                        <Card className="time-card">
+                            <CardContent className = "time-card">
+                                <Typography variant="h4" gutterBottom>
+                                    { this.state.remainingTime.minutes} 
+                                </Typography>
+                                <Typography>
+                                    Minutos
+                                </Typography>
+                            </CardContent>
+                        </Card>
+                    </Grid>
 
-                <Grid item xs={12} md={3}>     
-                    <Card className="time-card">
-                        <CardContent className = "time-card">
-                            <Typography variant="h4" gutterBottom>
-                                { this.state.remainingTime.hours} 
-                            </Typography>
-                            <Typography>
-                                Segundos
-                            </Typography>
-                        </CardContent>
-                    </Card>
+                    <Grid item xs={12} md={3}>     
+                        <Card className="time-card">
+                            <CardContent className = "time-card">
+                                <Typography variant="h4" gutterBottom>
+                                    { this.state.remainingTime.seconds} 
+                                </Typography>
+                                <Typography>
+                                    Segundos
+                                </Typography>
+                            </CardContent>
+                        </Card>
+                    </Grid>
                 </Grid>
+                <LinearProgress 
+                    variant="determinate" 
+                    color="secondary"
+                    valueBuffer={this.state.partyDay}
+                    value={ this.state.currentDay } />
+            </div>
 
-            </Grid>
         );
     }
 }
